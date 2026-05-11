9fa5cd01976ba7c5e427c4bbf9fa79f034f6b887
ichsan <ichsanfyudika@gmail.com>
2026-05-11 22:46:14 +0700

    Initial commit for ROS2 midterm project

---
+++ src/my_robot/launch/robot_launch.py
@@ -0,0 +1,59 @@
+import os
+import xacro
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    package_name = 'my_robot'
+    robot_name = 'differential_drive_robot'
+
+    pkg_path = get_package_share_directory(package_name)
+    xacro_file = os.path.join(pkg_path, 'model', 'robot.xacro')
+    bridge_yaml = os.path.join(pkg_path, 'config', 'gz_bridge.yaml')
+
+    # Xacro
+    robot_description_xml = xacro.process_file(xacro_file).toxml()
+
+    # Robot State Publisher
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'robot_description': robot_description_xml, 'use_sim_time': True}]
+    )
+
+    # Gazebo Sim
+    gazebo_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
+        launch_arguments={'gz_args': '-r empty.sdf'}.items(),
+    )
+
+    # Spawn Robot
+    node_spawn_robot = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=[
+            '-name', robot_name,
+            '-string', robot_description_xml,
+            '-z', '0.5'
+        ],
+        output='screen',
+    )
+
+    # Bridge
+    node_bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=['--ros-args', '-p', f'config_file:={bridge_yaml}'],
+        output='screen'
+    )
+
+    return LaunchDescription([
+        gazebo_launch,
+        node_spawn_robot,
+        node_robot_state_publisher,
+        node_bridge
+    ])
